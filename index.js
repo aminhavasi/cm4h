82721e53ee518e4717035d4fe4917c933dd9aefa
amin havasi <pau.ahq@gmail.com>
2021-01-22 06:52:04 +0330

    ok

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-salam;
+const http = require('http');
+const express = require('express');
+const app = express();
+const httpServer = http.createServer(app);
+require('dotenv').config({
+    path: './config/config.env',
+});
+
+const port = process.env.PORT || 8080;
+httpServer.listen(port, () => {
+    if (process.env.NODE_ENV === 'development') {
+        console.log(`servr is running on port ${port}`);
+    }
+});
